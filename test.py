61e12eada2bef1c50dd4d9a94ae718b64b20e332
sergebb <thereisalogin@gmail.com>
2014-01-15 23:20:00 +0400

    test.py changed to compare performance

--- test.py
+++ test.py
@@ -6,19 +6,23 @@ import scipy as sp
 def ccf(data):
     return np.array([sp.correlate(p, np.concatenate((p,p))) for p in data])
 
-z = np.zeros(100).reshape(10,10)
-for i in range(10):
-	for j in range(10):
+def corr(data):
+    return libcorr.Correlate(data)
+    
+
+z = np.zeros(1000000).reshape(1000,1000)
+for i in range(1000):
+	for j in range(1000):
 		z[i,j]=i^j
 
-z = np.array([[2.0,3.0,1.0,4.0,1.3,4.5,6.4,7.6]])
+#z = np.array([[2.0,3.0,1.0,4.0,1.3,4.5,6.4,7.6]])
 
-print libcorr.greet()
-print 2,libcorr.square(2)
+#print libcorr.greet()
+#print 2,libcorr.square(2)
 print z
 x = np.array(z,dtype=float)
-x = libcorr.Correlate(x)
-y = ccf(z)
-print x
+z = ccf(x)
+y = corr(x)
+#print x
 print z
 print y
